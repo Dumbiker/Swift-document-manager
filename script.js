d83d0f9b6c9f37b21931b8fb96d2cba7d46967c9
Webgames <Dumbiker2@gmail.com>
2025-10-19 19:39:45 -0500

    Add event listeners

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('document-btn').addEventListener('click', function() {
+    createDocument();
+});
+
+document.getElementById('spreadsheet-btn').addEventListener('click', function() {
+    createSpreadsheet();
+});
+
+function createDocument() {
+    // Logic to create a document
+    alert('Document creation initiated.');
+}
+
+function createSpreadsheet() {
+    // Logic to create a spreadsheet
+    alert('Spreadsheet creation initiated.');
+}
